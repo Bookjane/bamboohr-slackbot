62908058943ba9bc86bdff9f830cd4494b992342
matt <matthew.jay.wong@gmail.com>
2022-01-28 10:39:23 -0500

    fix slack daily update

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const server = app.listen(PORT, () => {
     app.settings.env
   );
 
-  schedule.schedule('30 10 * * 1-5', slackDailyUpdates, {
+  schedule.schedule('45 10 * * 1-5', slackDailyUpdates, {
     timezone: 'America/New_York'
   });
 });
